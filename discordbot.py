083f8e0d9636298d405d636ba7db2b6e12c2fa59
jpatel20 <jay_patel20@hotmail.com>
2018-02-18 10:53:35 -0500

    help command descriptions

--- discordbot.py
+++ discordbot.py
@@ -11,20 +11,17 @@ import os
 bot = commands.Bot(command_prefix='!', description='description here')
 client = discord.Client()
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
 @bot.command()
 async def say(*,something):
-    em = discord.Embed(title=None,description=something)
+    """ !say something, to echo something 
+    """
+    em = discord.Embed(title="!say",description=something, colour=discord.Colour(0xFFFFFF))
     await bot.say(embed=em)
 
 @bot.group(pass_context=True)
 async def scores(ctx): # working
+    """ !scores or !scores team_name, for daily scores
+    """
     if ctx.invoked_subcommand is None:
         today = datetime.datetime.now()
         games = mlbgame.day(today.year, today.month, today.day, home=None, away=None)
@@ -47,8 +44,10 @@ async def scores_team(*,team): # working
 
 @bot.command()
 async def standings():   # not working
-    #today=datetime.datetime(2017, 10, 6, 0, 59, 6, 838799)
-    today = datetime.datetime.now()
+    """ !standings, not working atm 
+    """
+    today=datetime.datetime(2018, 2, 23, 0, 59, 6, 838799)
+    #today = datetime.datetime.now()
     standings = standings(today)
     await bot.say(standings)
 
