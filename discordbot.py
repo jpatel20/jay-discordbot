90b15851f3edb439d6772f6dc868ccbf2374d508
jpatel20 <jay_patel20@hotmail.com>
2018-02-28 20:37:45 -0500

    Added emojify command

--- discordbot.py
+++ discordbot.py
@@ -51,6 +51,35 @@ async def standings():   # not working
     standings = standings(today)
     await bot.say(standings)
 
+@bot.command()
+async def emojify(*,something):
+    """ !emojify something, to echo something with emoji letters/numbers
+    """    
+    output = ""
+    for i, c in enumerate(something):
+        if c == "0":
+            output += ":zero:"
+        elif c == "1":
+            output += ":one:"
+        elif c == "2":
+            output += ":two:"		
+        elif c == "3":
+            output += ":three:"
+        elif c == "4":
+            output += ":four:"
+        elif c == "5":
+            output += ":five:"
+        elif c == "6":
+            output += ":six:"
+        elif c == "7":
+            output += ":seven:"
+        elif c == "8":
+            output += ":eight:"
+        elif c == "9":
+            output += ":nine:"
+        else:
+            output += ":regional_indicator_" + c.lower() + ":"
+    await bot.say(output)
 
 bot.run(os.getenv('TOKEN'))
 
